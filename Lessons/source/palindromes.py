3b50a7baf1a2fc5376d32cf9ace08e36d0d0b37e
Connor Cahill <connorcahill913@gmail.com>
2019-04-08 20:19:29 -0700

    [Adds]: palindromes recursive function. [Tests Passing]

--- Lessons/source/palindromes.py
+++ Lessons/source/palindromes.py
@@ -13,7 +13,7 @@ def is_palindrome(text):
     # implement is_palindrome_iterative and is_palindrome_recursive below, then
     # change this to call your implementation to verify it passes all tests
     assert isinstance(text, str), 'input is not a string: {}'.format(text)
-    return is_palindrome_iterative(text)
+    return is_palindrome_recursive(text)
     # return is_palindrome_recursive(text)
 
 
@@ -48,11 +48,24 @@ def is_palindrome_iterative(text):
 
 
 def is_palindrome_recursive(text, left=None, right=None):
-    # TODO: implement the is_palindrome function recursively here
-    pass
-    # once implemented, change is_palindrome to call is_palindrome_recursive
-    # to verify that your iterative implementation passes all tests
-
+    # if left is none function was just called
+    # skip down to recursion
+    if left is not None:
+        # base case: all items have been searched
+        # text is a palindrome
+        if left > right:
+            return True
+        # letters do not match
+        if text[left].lower() != text[right].lower():
+            # item is not letter
+            # keep skipping until it is
+            if not text[left].isalpha():
+                return is_palindrome_recursive(text, left + 1, right)
+            if not text[right].isalpha():
+                return is_palindrome_recursive(text, left, right - 1)
+            return False
+        return is_palindrome_recursive(text, left + 1, right - 1)
+    return is_palindrome_recursive(text, 0, len(text) - 1)
 
 def main():
     import sys

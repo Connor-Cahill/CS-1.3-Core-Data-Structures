9dddb0ac6c60e2b8784d9cef264f4e517a626967
connor cahill <connorcahill913@gmail.com>
2019-04-15 19:41:07 +0000

    [Adds]: methods for queue including empty, length, enqueue

--- Lessons/source/queue.py
+++ Lessons/source/queue.py
@@ -11,6 +11,11 @@ class LinkedQueue(object):
         """Initialize this queue and enqueue the given items, if any."""
         # Initialize a new linked list to store the items
         self.list = LinkedList()
+        # holds count of how many elements are in queue (keeps length method constant)
+        self.size = 0
+        # set head and tail of Queue
+        self.head = None
+        self.tail = None
         if iterable is not None:
             for item in iterable:
                 self.enqueue(item)
@@ -21,16 +26,31 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.head is None # returns true if nothing is in head
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return self.size # returns value of queue's size property
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        # create new node with given item
+        new_tail =  Node(item)
+        self.size += 1 # increment size counter
+        # check that list is not empty
+        if not self.is_empty():
+            # grab current tail in queue
+            current_tail = self.tail
+            self.tail = new_tail
+            # set the old tail's next to point towards new tail
+            current_tail.next = new_tail
+        else: # queue was empty:
+
+            # when queue is empty we need to
+            # add the new item as the head and tail of our queue
+            self.head = new_tail
+            self.tail = new_tail
 
     def front(self):
         """Return the item at the front of this queue without removing it,
